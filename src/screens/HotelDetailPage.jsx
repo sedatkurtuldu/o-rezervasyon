50aedd469be747978e467fb7010c39edcc6e60c7
sedatkurtulduELSA <sdt_krtldu@hotmail.com>
2024-02-19 00:44:04 +0300

    HotelDetailPage tamamlandı.

--- src/screens/HotelDetailPage.jsx
+++ src/screens/HotelDetailPage.jsx
@@ -15,7 +15,7 @@ import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import MapView, { Marker } from "react-native-maps";
-import * as Location from "expo-location";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 const width = Dimensions.get("window").width;
 
@@ -65,6 +65,19 @@ const HotelDetailPage = ({ route }) => {
     setShowFullDescription(!showFullDescription);
   };
 
+  const adjustHotelLocationOnMap = () => {
+    const { latitude, longitude } = hotelRegion;
+    mapRef.current.animateToRegion(
+      {
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      1000
+    );
+  };
+
   const renderFlatListItem = ({ item }) => {
     return (
       <View style={styles.bedContainer}>
@@ -81,130 +94,163 @@ const HotelDetailPage = ({ route }) => {
   };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        activeOpacity={0.6}
-        style={styles.headerBackButton}
-      >
-        <AntDesign
-          style={styles.leftCircleIcon}
-          name="left"
-          size={18}
-          color="black"
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.favIcon} onPress={handleFavIconPress}>
-        {isFavorite ? (
-          <View style={{ top: 36, right: 10 }}>
-            <Ionicons name={"heart"} size={30} color={"#e81f89"} />
-          </View>
-        ) : (
-          <View style={{ top: 36, right: 10 }}>
-            <View>
-              <Ionicons name={"heart"} size={30} color={"gray"} />
+    <>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          activeOpacity={0.6}
+          style={styles.headerBackButton}
+        >
+          <AntDesign
+            style={styles.leftCircleIcon}
+            name="left"
+            size={18}
+            color="black"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.favIcon} onPress={handleFavIconPress}>
+          {isFavorite ? (
+            <View style={{ top: 36, right: 10 }}>
+              <Ionicons name={"heart"} size={30} color={"#e81f89"} />
             </View>
-            <View style={{ position: "absolute" }}>
-              <Ionicons name={"heart-outline"} size={30} color={"white"} />
-            </View>
-          </View>
-        )}
-      </TouchableOpacity>
-      <Carousel
-        loop
-        width={width}
-        height={width / 1.5}
-        autoPlay={true}
-        data={hotelImages}
-        scrollAnimationDuration={1000}
-        renderItem={({ item }) => (
-          <Image source={{ uri: item.imageUrl }} style={styles.image} />
-        )}
-        panGestureHandlerProps={{
-          activeOffsetX: [-10, 10],
-        }}
-      />
-      <View style={styles.infoContainer}>
-        <Text style={styles.hotelNameText}>{data.name}</Text>
-        <Text style={styles.cityText}>
-          {data.district}, {data.city}
-        </Text>
-        {roomTypes.length > 0 && (
-          <View style={styles.roomNameContainer}>
-            {roomTypes.map((room, index) => (
-              <Text key={room.id}>
-                {room.RoomName}
-                {index !== roomTypes.length - 1 && ","}
-              </Text>
-            ))}
-            <Text>Odalar</Text>
-          </View>
-        )}
-        <View style={styles.line}></View>
-        <View>
-          {data.description.length > 250 ? (
-            showFullDescription ? (
-              <Text>{data.description}</Text>
-            ) : (
-              <Text>
-                {data.description.substring(0, 250)}...
-                <Text
-                  style={{ color: "#595959", textDecorationLine: "underline" }}
-                  onPress={toggleDescription}
-                >
-                  Daha Fazla Göster
-                </Text>
-              </Text>
-            )
           ) : (
-            <Text>{data.description}</Text>
+            <View style={{ top: 36, right: 10 }}>
+              <View>
+                <Ionicons name={"heart"} size={30} color={"gray"} />
+              </View>
+              <View style={{ position: "absolute" }}>
+                <Ionicons name={"heart-outline"} size={30} color={"white"} />
+              </View>
+            </View>
           )}
-          {data.description.length > 250 && showFullDescription && (
-            <Text
-              style={{ color: "#595959", textDecorationLine: "underline" }}
-              onPress={toggleDescription}
-            >
-              Daha Az Göster
-            </Text>
+        </TouchableOpacity>
+        <Carousel
+          loop
+          width={width}
+          height={width / 1.5}
+          autoPlay={true}
+          data={hotelImages}
+          scrollAnimationDuration={1000}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item.imageUrl }} style={styles.image} />
           )}
-        </View>
-        <View style={styles.line}></View>
-        <View style={styles.bedTextView}>
-          <Text style={styles.bedText}>Odalarımız</Text>
-        </View>
-        <FlatList
-          data={roomTypes}
-          horizontal
-          renderItem={renderFlatListItem}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ gap: 10 }}
-          showsHorizontalScrollIndicator={false}
+          panGestureHandlerProps={{
+            activeOffsetX: [-10, 10],
+          }}
         />
-        <View style={styles.line}></View>
-        <View>
-          <Text style={styles.bedText}>Konumumuz</Text>
-          <Text style={{ marginTop: 2, marginBottom: 10 , fontSize: 14, fontWeight: '400' }}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.hotelNameText}>{data.name}</Text>
+          <Text style={styles.cityText}>
             {data.district}, {data.city}
           </Text>
-        </View>
-        <MapView
-          style={styles.map}
-          ref={mapRef}
-          initialRegion={hotelRegion}
-          showsUserLocation={true}
-        >
-          <Marker
-            key={data.id}
-            coordinate={{
-              latitude: data.latitude,
-              longitude: data.longitude,
-            }}
-            title={data.name}
-            description={data.price.toString()}
+          {roomTypes.length > 0 && (
+            <View style={styles.roomNameContainer}>
+              {roomTypes.map((room, index) => (
+                <Text key={room.id}>
+                  {room.RoomName}
+                  {index !== roomTypes.length - 1 && ","}
+                </Text>
+              ))}
+              <Text>Odalar</Text>
+            </View>
+          )}
+          <View style={styles.line}></View>
+          <View>
+            {data.description.length > 250 ? (
+              showFullDescription ? (
+                <Text>{data.description}</Text>
+              ) : (
+                <Text>
+                  {data.description.substring(0, 250)}...
+                  <Text
+                    style={{
+                      color: "#595959",
+                      textDecorationLine: "underline",
+                    }}
+                    onPress={toggleDescription}
+                  >
+                    Daha Fazla Göster
+                  </Text>
+                </Text>
+              )
+            ) : (
+              <Text>{data.description}</Text>
+            )}
+            {data.description.length > 250 && showFullDescription && (
+              <Text
+                style={{ color: "#595959", textDecorationLine: "underline" }}
+                onPress={toggleDescription}
+              >
+                Daha Az Göster
+              </Text>
+            )}
+          </View>
+          <View style={styles.line}></View>
+          <View style={styles.bedTextView}>
+            <Text style={styles.bedText}>Nasıl bir oda arıyorsunuz?</Text>
+          </View>
+          <FlatList
+            data={roomTypes}
+            horizontal
+            renderItem={renderFlatListItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{ gap: 10 }}
+            showsHorizontalScrollIndicator={false}
           />
-        </MapView>
+          <View style={styles.line}></View>
+          <View>
+            <Text style={styles.bedText}>Konumumuz</Text>
+            <Text
+              style={{
+                marginTop: 2,
+                marginBottom: 10,
+                fontSize: 14,
+                fontWeight: "400",
+              }}
+            >
+              {data.district}, {data.city}
+            </Text>
+          </View>
+          <MapView
+            style={styles.map}
+            ref={mapRef}
+            initialRegion={hotelRegion}
+            showsUserLocation={true}
+          >
+            <Marker
+              key={data.id}
+              coordinate={{
+                latitude: data.latitude,
+                longitude: data.longitude,
+              }}
+              title={data.name}
+              description={data.price.toString()}
+            />
+          </MapView>
+          <TouchableOpacity
+            onPress={adjustHotelLocationOnMap}
+            activeOpacity={0.4}
+            style={styles.locationIcon}
+          >
+            <SimpleLineIcons name="location-pin" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <View style={styles.reservationContainer}>
+        <View style={styles.reservationTextContainer}>
+          <View style={styles.reservationPriceContainer}>
+            <Text style={styles.priceText}>{data.price} ₺</Text>
+            <Text style={{ color: '#595959' }}>gece</Text>
+          </View>
+          <Text style={{ textDecorationLine: 'underline' }}>20-26 Nisan</Text>
+        </View>
+        <View style={styles.reservationButtonContainer}>
+          <TouchableOpacity style={styles.reservationButton} activeOpacity={0.6}>
+            <Text style={styles.reservationText}>Rezerve Edin</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </ScrollView>
+    </>
   );
 };
 
@@ -290,6 +336,55 @@ const styles = StyleSheet.create({
   map: {
     width: width * 0.93,
     height: 200,
-    marginBottom: 40
+    marginBottom: 40,
   },
+  locationIcon: {
+    position: "absolute",
+    alignSelf: "center",
+    bottom: 70,
+    right: 10,
+    backgroundColor: "white",
+    opacity: 0.8,
+    padding: 6,
+  },
+  reservationContainer: {
+    backgroundColor: "white",
+    height: 75,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  reservationTextContainer: {
+    marginHorizontal: 20
+  },
+  reservationPriceContainer: {
+    flexDirection: 'row',
+    gap: 8
+  },
+  priceText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  reservationButtonContainer: {
+    marginHorizontal: 20,
+  },
+  reservationButton: {
+    backgroundColor: '#cb1d53',
+    padding: 10,
+    borderRadius: 10,
+  },
+  reservationText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16
+  }
 });
