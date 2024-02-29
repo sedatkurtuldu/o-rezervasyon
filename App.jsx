17baafa62c9762ebfe9ce920552cdacec9f70229
MURAT CANKAT KÖK <mcankatkok@gmail.com>
2024-03-01 01:21:57 +0300

    font remove

--- App.jsx
+++ App.jsx
@@ -1,16 +1,10 @@
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import store from './src/app/store';
-import Router from './src/routes/Router';
-import { useFonts } from 'expo-font';
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import store from "./src/app/store";
+import Router from "./src/routes/Router";
 
 export default function App() {
-
-  const [fontsLoaded] = useFonts({
-    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
-  });
-
   return (
     <Provider store={store}>
       <NavigationContainer>
